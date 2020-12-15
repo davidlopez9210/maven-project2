dbf1f5376e732790af0a4b3e329aaed8176a4cb5
David López <davidlopez9210@gmail.com>
2020-12-15 01:16:02 +0000

    Se hacen algunos ajustes de checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,9 +13,10 @@ public class Greeter {
   }
 
   /**
-  * This is a method
+  * @param someone the name of a person
+  * @return greeting string
   */
-  public String greet(String someone) {
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
